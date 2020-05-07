ebf36151b11a0789367e2e673c82a1bbfaca3e3c
Tien Do Nam <dev.tien.donam@gmail.com>
2020-05-07 18:07:22 +0200

    refactoring: move images to CloudController

--- src/main/kotlin/de/stephaneum/spring/controller/CloudController.kt
+++ src/main/kotlin/de/stephaneum/spring/controller/CloudController.kt
@@ -3,10 +3,11 @@ package de.stephaneum.spring.controller
 import de.stephaneum.spring.Session
 import de.stephaneum.spring.database.FileRepo
 import de.stephaneum.spring.helper.FileService
+import de.stephaneum.spring.scheduler.ConfigScheduler
+import de.stephaneum.spring.scheduler.Element
 import de.stephaneum.spring.security.JwtService
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import org.springframework.stereotype.Controller
 import org.springframework.util.FileCopyUtils
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam
 import java.io.IOException
 import java.io.InputStreamReader
 import java.io.UncheckedIOException
+import javax.servlet.http.HttpServletRequest
 
 @Controller
 class CloudController (
         private val fileService: FileService,
         private val jwtService: JwtService,
+        private val configScheduler: ConfigScheduler,
         private val fileRepo: FileRepo
 ) {
 
@@ -61,5 +64,36 @@ class CloudController (
                     }
                     .body(resource)
         }
+    }
+
+    @GetMapping("/api/images/{fileName}")
+    fun image(@PathVariable fileName: String, request: HttpServletRequest): Any {
+
+        // get file content
+        val resource = configScheduler.get(Element.fileLocation)?.let { location ->
+            fileService.loadFileAsResource("$location/$fileName")
+        } ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).build<Void>()
+
+        // check 304 not modified
+        val lastModified = (resource.lastModified() / 1000) * 1000 // ignore millis
+        if(lastModified <= request.getDateHeader("If-Modified-Since")) {
+            return ResponseEntity.status(HttpStatus.NOT_MODIFIED).headers(HttpHeaders().apply {
+                setDate("Last-Modified", lastModified)
+            }).build<Void>()
+        }
+
+        // mime
+        val mime = fileService.getMime(fileName.substring(fileName.lastIndexOf('.')+1).toLowerCase())
+        if(!fileService.isImage(mime)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build<Void>()
+
+        // success request
+        return ResponseEntity.ok()
+                .headers(HttpHeaders().apply {
+                    setDate("Last-Modified", lastModified)
+                })
+                .cacheControl(CacheControl.empty().cachePublic())
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.parseMediaType(mime))
+                .body(resource)
     }
 }
--- src/main/kotlin/de/stephaneum/spring/rest/PublicAPI.kt
+++ src/main/kotlin/de/stephaneum/spring/rest/PublicAPI.kt
@@ -5,24 +5,19 @@ import de.stephaneum.spring.database.EMPTY_USER
 import de.stephaneum.spring.database.PostRepo
 import de.stephaneum.spring.database.ROLE_ADMIN
 import de.stephaneum.spring.database.ROLE_NO_LOGIN
-import de.stephaneum.spring.helper.FileService
 import de.stephaneum.spring.helper.MenuService
 import de.stephaneum.spring.rest.objects.Response
 import de.stephaneum.spring.scheduler.ConfigScheduler
 import de.stephaneum.spring.scheduler.Element
-import org.springframework.http.*
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/api")
 class PublicAPI (
         private val configScheduler: ConfigScheduler,
         private val menuService: MenuService,
-        private val fileService: FileService,
         private val postRepo: PostRepo
 ) {
 
@@ -39,36 +34,5 @@ class PublicAPI (
             else                                                        -> null
         }
         return Response.Info(user, menuService.getPublic(), copyright, plan, history, euSa, unapproved)
-    }
-
-    @GetMapping("/images/{fileName}")
-    fun image(@PathVariable fileName: String, request: HttpServletRequest): Any {
-
-        // get file content
-        val resource = configScheduler.get(Element.fileLocation)?.let { location ->
-            fileService.loadFileAsResource("$location/$fileName")
-        } ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).build<Void>()
-
-        // check 304 not modified
-        val lastModified = (resource.lastModified() / 1000) * 1000 // ignore millis
-        if(lastModified <= request.getDateHeader("If-Modified-Since")) {
-            return ResponseEntity.status(HttpStatus.NOT_MODIFIED).headers(HttpHeaders().apply {
-                setDate("Last-Modified", lastModified)
-            }).build<Void>()
-        }
-
-        // mime
-        val mime = fileService.getMime(fileName.substring(fileName.lastIndexOf('.')+1).toLowerCase())
-        if(!fileService.isImage(mime)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build<Void>()
-
-        // success request
-        return ResponseEntity.ok()
-                .headers(HttpHeaders().apply {
-                    setDate("Last-Modified", lastModified)
-                })
-                .cacheControl(CacheControl.empty().cachePublic())
-                .contentLength(resource.contentLength())
-                .contentType(MediaType.parseMediaType(mime))
-                .body(resource)
     }
 }
