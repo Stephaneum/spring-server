6480233466851dac044f2506782972aa19e81d6e
Tien Do Nam <dev.tien.donam@gmail.com>
2020-05-14 18:31:08 +0200

    fix(router): login load on init

--- src/main/vue/src/router.js
+++ src/main/vue/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -13,8 +14,7 @@ const routes = [
   {
     path: "/login",
     name: "login",
-    component: () =>
-      import(/* webpackChunkName: "login" */ "./views/Login.vue"),
+    component: Login,
   },
   {
     path: "/config-manager",
