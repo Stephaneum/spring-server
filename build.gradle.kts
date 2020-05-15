79060f262c9923e5c063091cdb7556074af42a9d
Tien Do Nam <dev.tien.donam@gmail.com>
2020-05-15 21:24:21 +0200

    fix: update kotlin and spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.6.RELEASE"
+	id("org.springframework.boot") version "2.2.7.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 
-	val kotlinVersion = "1.3.71"
+	val kotlinVersion = "1.3.72"
 	kotlin("jvm") version kotlinVersion
 	kotlin("plugin.spring") version kotlinVersion
 	kotlin("plugin.jpa") version kotlinVersion
