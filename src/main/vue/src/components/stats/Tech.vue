a069489dad3a20299b00b07cfa82e7053a788617
Tien Do Nam <dev.tien.donam@gmail.com>
2020-05-18 22:20:03 +0200

    feat: add link to tech

--- src/main/vue/src/components/stats/Tech.vue
+++ src/main/vue/src/components/stats/Tech.vue
@@ -1,6 +1,8 @@
 <template>
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center">
-        <img :src="require('../../assets/img/tech/'+img)" :style="{ 'width': width+'px' }">
+        <a :href="link" target="_blank">
+            <img :src="require('../../assets/img/tech/'+img)" :style="{ 'width': width+'px' }">
+        </a>
         <p style="margin-top: 30px; font-size: 1.1rem; background-color: #dcedc8; border-radius: 20px; text-align: center; padding: 5px 20px 5px 20px">{{ title }}</p>
     </div>
 </template>
@@ -8,6 +10,6 @@
 <script>
 export default {
     name: 'Tech',
-    props: ['img', 'width', 'title', 'info']
+    props: ['img', 'link', 'width', 'title', 'info']
 }
 </script>
--- src/main/vue/src/views/Stats.vue
+++ src/main/vue/src/views/Stats.vue
@@ -13,10 +13,10 @@
 
     <StatsPanel title="Technologien" icon="settings">
       <div style="margin: 30px 0 20px 0; display: flex; align-items: end; justify-content: space-evenly">
-        <Tech title="Frontend-Framework" name="Vue" img="vue.png" width="200" info="Blablabla"></Tech>
-        <Tech title="Backend-Framework" name="Spring" img="spring.png" width="130" info="Blablabla"></Tech>
-        <Tech title="Datenbanksystem" name="MySQL" img="mysql.png" width="130" info="Blablabla"></Tech>
-        <Tech title="Betriebssystem" name="Ubuntu" img="ubuntu.png" width="200" info="Blablabla"></Tech>
+        <Tech title="Frontend-Framework" name="Vue" img="vue.png" width="200" info="Blablabla" link="https://vuejs.org"></Tech>
+        <Tech title="Backend-Framework" name="Spring" img="spring.png" width="130" info="Blablabla" link="https://spring.io"></Tech>
+        <Tech title="Datenbanksystem" name="MySQL" img="mysql.png" width="130" info="Blablabla" link="https://www.mysql.com"></Tech>
+        <Tech title="Betriebssystem" name="Ubuntu" img="ubuntu.png" width="200" info="Blablabla" link="https://ubuntu.com"></Tech>
       </div>
     </StatsPanel>
 
