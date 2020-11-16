285f81d1ec7cce9d48887562f10921a0f0cd113a
Tien Do Nam <dev.tien.donam@gmail.com>
2020-11-16 22:47:15 +0100

    feat: bump spring to 2.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.3.RELEASE"
+	id("org.springframework.boot") version "2.3.6.RELEASE"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 
 	val kotlinVersion = "1.3.72"
