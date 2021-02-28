e7ba191a5a99c5ea00f7e7f13468cc0b3410e248
Tien Do Nam <dev.tien.donam@gmail.com>
2021-02-28 18:06:52 +0100

    deps: update spring and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.2"
+	id("org.springframework.boot") version "2.4.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 
-	val kotlinVersion = "1.4.21"
+	val kotlinVersion = "1.4.31"
 	kotlin("jvm") version kotlinVersion
 	kotlin("plugin.spring") version kotlinVersion
 	kotlin("plugin.jpa") version kotlinVersion
