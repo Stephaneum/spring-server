f2dad1c2854be279945ac5955c99b89f4e4babce
Tien Do Nam <dev.tien.donam@gmail.com>
2022-12-19 22:38:34 +0100

    feat: login with user with the same email

--- src/main/kotlin/de/stephaneum/spring/security/CustomOidcUserService.kt
+++ src/main/kotlin/de/stephaneum/spring/security/CustomOidcUserService.kt
@@ -1,11 +1,9 @@
 package de.stephaneum.spring.security
 
 import de.stephaneum.spring.Session
-import de.stephaneum.spring.database.CodeRepo
-import de.stephaneum.spring.database.ROLE_STUDENT
-import de.stephaneum.spring.database.User
-import de.stephaneum.spring.database.UserRepo
+import de.stephaneum.spring.database.*
 import de.stephaneum.spring.helper.CodeService
+import de.stephaneum.spring.helper.LogService
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
@@ -16,28 +14,22 @@ class CustomOidcUserService (
     private val userRepo: UserRepo,
     private val codeRepo: CodeRepo,
     private val codeService: CodeService,
+    private val logService: LogService,
 ) : OidcUserService() {
 
     @Override
     override fun loadUser(userRequest: OidcUserRequest): OidcUser {
-        val oidcUser = super.loadUser(userRequest);
-
-        processOidcUser(userRequest, oidcUser);
-
-        return oidcUser;
+        val oidcUser = super.loadUser(userRequest)
+        processOidcUser(oidcUser)
+        return oidcUser
     }
 
-    private fun processOidcUser(userRequest: OidcUserRequest, oidcUser: OidcUser) {
-
-        // create user in database
-        if (!userRepo.existsByOpenIdSubject(oidcUser.subject)) {
-
-            // generare new code
-            //TODO: get right role!
-            val code = codeRepo.save(
-                codeService.generateCode(ROLE_STUDENT))
-
-            val user = userRepo.save(User(
+    private fun processOidcUser(oidcUser: OidcUser) {
+        var user = userRepo.findByEmail(oidcUser.email)
+        if (user == null) {
+            // register new user
+            val code = codeRepo.save(codeService.generateCode(ROLE_STUDENT))
+            user = userRepo.save(User(
                 code = code,
                 firstName = oidcUser.givenName,
                 lastName = oidcUser.familyName,
@@ -45,10 +37,11 @@ class CustomOidcUserService (
                 openIdSubject = oidcUser.subject,
                 openId = true,
             ))
-            Session.get().user = user
+            logService.log(EventType.REGISTER, user, "Open ID Login")
         } else {
-            val user = userRepo.findByOpenIdSubject(oidcUser.subject)
-            Session.get().user = user
+            // do nothing, just take the user with the same email
+            logService.log(EventType.LOGIN, user, "Open ID Login")
         }
+        Session.get().user = user
     }
 }
