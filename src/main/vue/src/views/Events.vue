71e775429760b97cb3ae089a9ea02000b9de18f8
Tien Do Nam <dev.tien.donam@gmail.com>
2023-08-28 02:11:24 +0200

    feat: add tooltip to events

--- src/main/vue/src/views/Events.vue
+++ src/main/vue/src/views/Events.vue
@@ -1,25 +1,6 @@
 <template>
   <CenterLayout title="Termine" :plan="info.plan" :history="info.history" :eu-sa="info.euSa" hide-title="true">
-    <FullCalendar
-        ref="fullCalendar"
-        defaultView="dayGridMonth"
-        locale="de"
-        event-color="rgb(220, 237, 200)"
-        event-text-color="black"
-        :first-day="1"
-        :fixed-week-count="false"
-        :events="events"
-        :plugins="calendarPlugins"
-        :header="{
-            left: 'prev',
-            center: 'title',
-            right: 'next'
-        }"
-        :custom-buttons="{
-          prev: { click: prev },
-          next: { click: next }
-        }"
-    />
+    <FullCalendar :options="calendarOptions" />
 
     <h5 style="margin: 40px 0 20px 0">Alle Ereignisse aufgelistet:</h5>
     <div v-for="(e, index) in monthEvents" :key="'e'+index">
@@ -34,50 +15,68 @@
   import CenterLayout from "../components/CenterLayout";
   import FullCalendar from '@fullcalendar/vue'
   import dayGridPlugin from '@fullcalendar/daygrid'
-  import '@fullcalendar/core/main.css';
-  import '@fullcalendar/daygrid/main.css';
+  import tippy from 'tippy.js';
+  import 'tippy.js/dist/tippy.css';
 
   export default {
     name: 'Events',
     components: { FullCalendar, CenterLayout },
     props: ['info'],
     data: () => ({
-      events: null,
       calendarPlugins: [ dayGridPlugin ],
-      currYear: new Date().getFullYear(),
-      currMonth: new Date().getMonth() // 0 - 11
+      monthEvents: [],
+      calendarOptions: {
+        plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        locale: 'de',
+        weekends: true,
+        events: [],
+        headerToolbar: {
+          left: 'prev',
+          center: 'title',
+          right: 'next',
+        },
+        firstDay: 1,
+        fixedWeekCount: false,
+        eventColor: 'rgb(220, 237, 200)',
+        eventTextColor: 'black',
+        eventDidMount: function(info) {
+          let tooltipContent = info.event.extendedProps.tooltipContent;
+          if (tooltipContent) {
+            tippy(info.el, {
+              content: tooltipContent,
+              allowHTML: true, // allows for HTML content inside the tooltip
+              animation: 'fade', // use 'scale' or other animations if you want
+            });
+          }
+        },
+      },
     }),
     methods: {
-      prev: function() {
-        this.$refs.fullCalendar.getApi().prev();
-
-        this.currMonth--;
-        if (this.currMonth === -1) {
-          this.currYear--;
-          this.currMonth = 11;
-        }
+      setMonthEvents(year, month) {
+        this.monthEvents = this.calendarOptions.events.filter(event => {
+          const eventDate = new Date(event.start);
+          return eventDate.getFullYear() === year && eventDate.getMonth() === month;
+        });
       },
-      next: function() {
-        this.$refs.fullCalendar.getApi().next();
-
-        this.currMonth++;
-        if (this.currMonth === 12) {
-          this.currYear++;
-          this.currMonth = 0;
-        }
-      }
-    },
-    computed: {
-      monthEvents: function() {
-        if(!this.events)
-          return [];
-        return this.events.filter(e => e.startMoment.month() === this.currMonth && e.startMoment.year() === this.currYear);
-      }
     },
     async mounted() {
+      this.calendarOptions.datesSet = (arg) => {
+        const start = arg.start;
+        const end = arg.end;
+
+        const middleDate = new Date(start.getTime() + (end.getTime() - start.getTime()) / 2);
+
+        const year = middleDate.getFullYear();
+        const month = middleDate.getMonth(); // Remember, months are zero-indexed. So, 0 = January, 11 = December.
+
+        this.setMonthEvents(year, month);
+      }
       const events = await Axios.get('/api/events');
-      this.events = events.data;
-      this.events.forEach(e => {
+      events.data.forEach(e => {
+        e.extendedProps = {
+          tooltipContent: e.title,
+        };
         e.startMoment = moment(e.start);
         if (e.end) {
           e.endMoment = moment(e.end);
@@ -90,19 +89,34 @@
             e.timeString = e.startMoment.format('DD.MM, HH:mm');
         }
       });
+
+      this.calendarOptions.events = events.data;
+
+      const date = new Date();
+      const currYear = date.getFullYear();
+      const currMonth = date.getMonth();
+      this.setMonthEvents(currYear, currMonth);
     }
   }
 </script>
 
 <style>
-  .fc-button-primary {
-    color: #fff;
-    background-color: #1b5e20;
-    border-color: #1b5e20;
+  /* fc means fullcalendar, we modify the default UI here */
+
+  .fc-daygrid-day-number {
+    color: black;
   }
 
-  .fc-button-primary:hover {
-    background-color: #388e3c !important;
-    border-color: #388e3c !important;
+  .fc-col-header-cell-cushion {
+    color: black;
+  }
+
+  :root {
+    --fc-button-bg-color: #1b5e20;
+    --fc-button-active-bg-color: #388e3c;
+    --fc-button-border-color: #1b5e20;
+    --fc-button-text-color: #fff;
+    --fc-button-hover-bg-color: #388e3c;
+    --fc-button-hover-border-color: #388e3c;
   }
 </style>
